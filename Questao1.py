94bdc5607ff70c267914a0f71abf5ff656d63a13
LuizaBB <luizabezerrabastos@gmail.com>
2025-09-15 14:51:30 -0300

    Informações Grafo Questão 1
    Extração de número de vértices, número de arestas e colunas de arestas com vértice inicial, vértice final e custo

--- Questao1.py
+++ Questao1.py
@@ -1,4 +1,16 @@
 import pandas as pd
+from ImplementacoesGrafosPython import GraphAdjMatrix as GMatrix
 
 input_graph=pd.read_fwf('graph1.txt')
 print(input_graph) #print de teste
+nodesN=input_graph.iloc[0,0]
+edgesM=input_graph.iloc[0,1]
+print(nodesN, edgesM) #print de teste
+initialNode=input_graph.iloc[1:22, 0]
+finalNode=input_graph.iloc[1:22, 1]
+costNode=input_graph.iloc[1:22, 2]
+print(initialNode)
+print(finalNode)
+print(costNode)
+# print(input_graph.iloc[1:22, 0])
+# graph=GMatrix()
