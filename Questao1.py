c90500aef5d6147b465742172792303e4ec0cae0
LuizaBB <luizabezerrabastos@gmail.com>
2025-09-15 15:48:22 -0300

    Preenchimento do grafo
    Update na extração das informações de input (conversão das colunas lidas em listas, além da conversão float -> int na coluna de custos) e loop para adição das arestas obtidas, além de um print da matriz para confirmação.
    OBS: a matriz impressa está no tamanho n+1 X n+1 (13 X 13)

--- Questao1.py
+++ Questao1.py
@@ -2,15 +2,18 @@ import pandas as pd
 from ImplementacoesGrafosPython import GraphAdjMatrix as GMatrix
 
 input_graph=pd.read_fwf('graph1.txt')
-print(input_graph) #print de teste
+# print(input_graph) #print de teste
 nodesN=input_graph.iloc[0,0]
 edgesM=input_graph.iloc[0,1]
-print(nodesN, edgesM) #print de teste
-initialNode=input_graph.iloc[1:23, 0]
-finalNode=input_graph.iloc[1:23, 1]
-costNode=input_graph.iloc[1:23, 2]
-print(initialNode) #print de teste
-print(finalNode) #print de teste
-print(costNode) #print de teste
+# print(nodesN, edgesM) #print de teste
+initialNode=input_graph.iloc[1:23, 0].to_list()
+finalNode=input_graph.iloc[1:23, 1].to_list()
+costNode=input_graph.iloc[1:23, 2].astype(int).to_list()
+# print(type(costNode[1]))
+# print(initialNode, len(initialNode)) #print de teste
+# print(finalNode, len(finalNode)) #print de teste
+# print(costNode, len(costNode)) #print de teste
 graph=GMatrix(nodesN, directed=True)
-# for i in range (1, 22)
+for i in range (0, 22):
+    graph.addEdge(initialNode[i], finalNode[i], costNode[i])
+graph.print_matrix() #print de teste
