a9d01ebe69a380012754c5d68ddc8272fa8f396f
LuizaBB <luizabezerrabastos@gmail.com>
2025-09-15 14:56:49 -0300

    Update Questao1.py
    Acréscimo de 1 linha nas variáveis initialNode, finalNode e costNode (aumentando o intervalo para 1:23) para incluir todos os valores desejados de vértices e custos

--- Questao1.py
+++ Questao1.py
@@ -6,11 +6,11 @@ print(input_graph) #print de teste
 nodesN=input_graph.iloc[0,0]
 edgesM=input_graph.iloc[0,1]
 print(nodesN, edgesM) #print de teste
-initialNode=input_graph.iloc[1:22, 0]
-finalNode=input_graph.iloc[1:22, 1]
-costNode=input_graph.iloc[1:22, 2]
-print(initialNode)
-print(finalNode)
-print(costNode)
-# print(input_graph.iloc[1:22, 0])
-# graph=GMatrix()
+initialNode=input_graph.iloc[1:23, 0]
+finalNode=input_graph.iloc[1:23, 1]
+costNode=input_graph.iloc[1:23, 2]
+print(initialNode) #print de teste
+print(finalNode) #print de teste
+print(costNode) #print de teste
+graph=GMatrix(nodesN, directed=True)
+# for i in range (1, 22)
