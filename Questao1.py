53444bb46036742175170e3b8fb043650e6359c6
LuizaBB <luizabezerrabastos@gmail.com>
2025-09-18 08:50:38 -0300

    Update Questão1 Remoção de prints de teste
    Aplicação das respostas + remoção dos prints usados para testes

--- Questao1.py
+++ Questao1.py
@@ -27,58 +27,37 @@ def floydAlgorithm(graph):
                 D[i][j]=0
             if graph.M[i][j] != 0:
                 R[i][j]=j
-    print("print matriz declarada")
-    printMatrix(D) #print de teste
-    printMatrix(R) #print de teste
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
                 if D[i][k]+D[k][j]<D[i][j]:
                     D[i][j]=D[i][k]+D[k][j]
                     R[i][j]=R[i][k]
-    print("print matriz final")
-    printMatrix(D) #print de teste
-    printMatrix(R) #print de teste
     return D, R
 
 if __name__== "__main__":
     input_graph=pd.read_fwf('graph1.txt', header=None)
 
-    # print(input_graph) #print de teste
     nodesN=input_graph.iloc[0,0]
     edgesM=input_graph.iloc[0,1]
-    # print(nodesN, edgesM) #print de teste
     initialNode=input_graph.iloc[1:edgesM+1, 0].to_list()
     finalNode=input_graph.iloc[1:edgesM+1, 1].to_list()
     costNode=input_graph.iloc[1:edgesM+1, 2].astype(int).to_list()
 
-    # print(initialNode, len(initialNode)) #print de teste
-    # print(finalNode, len(finalNode)) #print de teste
-    # print(costNode, len(costNode)) #print de teste
 
     graph=GMatrix(nodesN, directed=True)
     for i in range (0, edgesM):
         graph.addEdge(initialNode[i], finalNode[i], costNode[i])
-    print("print matriz lida")
-    graph.print_matrix() #print de teste
 
     D, R = floydAlgorithm(graph)
 
     nodeSum=[]
     for i in range(1, graph.n+1):
         nodeSum.append(sumRow(D[i]))
-    print(nodeSum) #print de teste
 
     centralNode=nodeSum.index(min(nodeSum))+1 #nó central
-    print(centralNode) #print de teste
     centralNode_distanceList=D[centralNode][1: nodesN] #vetor de distâncias em relação ao central
-    print(centralNode_distanceList) #print de teste
     mostDistantNode=centralNode_distanceList.index(max(centralNode_distanceList))+2 #vértice mais longe do central
-    print(mostDistantNode) #print de teste
-    # candidatesMatrix=[None]*(nodesN+1)
-    # for i in range(0, nodesN+1):
-    #     candidatesMatrix[i]=D[i]
-    # printMatrix(candidatesMatrix)
     
     #respostas:
     print(f"Pela matriz dada:\n   Nó escolhido como central: {centralNode}\n   Distâncias em relação ao nó central: {centralNode_distanceList}\n   Vértice mais distante do central: {mostDistantNode}\n   Matriz de backtracking: ")
