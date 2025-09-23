8a7c03d9ba800a6ceb123e1523ee0495cff408dc
LuizaBB <luizabezerrabastos@gmail.com>
2025-09-23 17:19:58 -0300

    Update Questao3
    Leitura da entrada + prints de teste para obter o número de linhas e colunas do mapa

--- Questao3.py
+++ Questao3.py
@@ -15,3 +15,28 @@ Com isso:
     Não entendi a função dos pontos
         O ponto vai ser interpretado como substituição ao =, já que a documentação só fala de = mas não tem = no exemplo
 '''
+import pandas as pd
+from ImplementacoesGrafosPython import GraphAdjMatrix as GMatrix
+
+def printMatrix(matrix, k):
+    n=len(matrix[0])-1 #considerando matriz quadrada, considerando grafo com tamanho (0, N+1)
+    for i in range(k, n+k):
+        print(matrix[i][k: n+k])
+
+if __name__== "__main__":
+
+    Input=[]
+    with open("grid_example.txt", 'r') as input:
+        for line in input:
+            final_Line=line.strip("\n")
+            Input.append(final_Line)
+    inputMap=pd.DataFrame(Input)
+    print(inputMap)
+    print(inputMap.iloc[0,0])
+    print(inputMap.iloc[0,0][0])
+    print(inputMap.iloc[0,0][1])
+    rows=int(inputMap.iloc[0,0][0]+inputMap.iloc[0,0][1])
+    collumns=int(inputMap.iloc[0,0][3]+inputMap.iloc[0,0][4])
+    print(rows, collumns, type(rows)) #print de teste
+    # collumns=int(inputMap.iloc[0,1])
+    # print("Print da quantidade de linhas e colunas do mapa: ", rows, collumns);
