b86ea756863c38d7c13bee69e49a161c73ad03ef
LuizaBB <luizabezerrabastos@gmail.com>
2025-09-30 00:50:15 -0300

    Update ImplementacoesGrafosPython
    adição da implementação de grafos em lista de adjacencia para a questao 3

--- ImplementacoesGrafosPython.py
+++ ImplementacoesGrafosPython.py
@@ -189,3 +189,157 @@ class GraphAdjMatrix(GraphBase): #classe geral para controle de 1 grafo por matr
                 edgeElement={(j, i): self.M[j][i]}
                 edges.update(edgeElement)
     return edges
+  
+class GraphAdjList(GraphBase): #aqui a classe geral que vai representar o grafo em formato de lista
+#como é uma lista, o grafo tem 2 partes principais: as listas e os nós que compõe as listas
+  #como é python, a aplicação simplifica a parte de arrays e listas para apenas listas: téra uma lista princilap para os vértices e listas menores para os vizinhos
+#por causa dessa lógica é necessário formar ainda 2 classes dentro da classe do grafo: um para os nós (pois gerarão os objetos nós, que cada um carregará um vértice) e a classe lista (que vai gerar os objetos listas, visto que cada lista terá um papel diferente)
+#com uma classe principal e 2 subclasses se garante que as listas e nós alterados sejam pertencentes ao mesmo grafo.
+  class Node(object): #classe responsável pela criação e edição de objetos nós
+  #node é vértice em ingles, então o nome segue a mesma linha da classe Edge
+    """ Definition of node
+    ...
+
+    Attributes
+    ----------
+    value : int
+        Value of node
+    edge : Edge
+        Edge which the node represents.
+    next : Node
+        "Pointer" to next node
+
+    """
+    def __init__(self) -> None: #iniciação e declaração: à princípio está vazio pois quem vai preenchê-lo será a lista
+      self.value = None #declara o valor do vértice (próprio caso seja a lista principal ou vizinho se for a alista auxiliar)
+      self.edge = None #declara a aresta que representa
+      self.next = None #declara o "ponteiro" para o próximo nó, criando a ideia de lista
+
+    def __int__(self) -> int: # para facilitar a comparação e exibição 3método para retornar os valores contidos no nó
+      return self.value
+
+  class Edge(object): #classe de listas (chamada de Edges provavlmente porque são as arestas que fazem a conexão entre os vértices (?)) não concordo muito mas tudo bem
+    def __init__(self): #declaração (à princípio a lista está vazia)
+      self.v1, self.node1 = None, None #declara o vértice 1 (e seu respectivo nó)
+      self.v2, self.node2 = None, None #declara o vértice 2 (e seu respectivo nó)
+      #declarando os dois elementos da arestas, é possível fazer a ligação entre eles e, portanto, a lista (?)
+      #OBS:Como está dentro de um __init__ (assim como todos objetos de POO), chamar Edge() no código principal apenas faz o "alocamento de memória", mas não cria nem delacara essas variáveis automaticamente
+        #esse método sera utilizado por outros métodos ao longo da classe como apoio e organização
+
+  def __init__(self, n, directed = False, doublyLinkedList = False, iterateOverNode = False): #continuação da declaração
+    super().__init__(n, directed) #herda as definições do __init__ anterior (para não ser repetitivo)
+    self.L = [None]*(self.n + 1) #declara uma lista com o tamanho de vértices (a lista principal)
+      #puxa a ideia de índice especificada na matriz de adjacência
+    for i in range(1, self.n + 1): #para cada índice da lista principal
+      self.L[i] = GraphAdjList.Node() # head nodes #cria-se a lista auxiliar para os vizinhos de cada nó
+      #como se considera que os vértices do grafo são números consecutivos começando do 1, os índices é uma referência para mapear as listas
+
+    self.doublyLinkedList = doublyLinkedList #adiciona booleano se é não direcionado (lista duplamente encadeada) ou não (simplismente encadeada)
+      #imagino que, se for duplamente encadeada, utiliza esse booleano para estabelecer o "duplo" caminho
+    self.iterateOverNode = iterateOverNode # adiciona booleano de incluir ou não o próprio vértice em outras funções (como obter os vizinhos)
+
+
+  def addEdge(self, v, w): # método de adicionar arestas, recebendo os dois vértices que compõe a aresta
+  #o objetivo principal é adicionar aresta (addEdge), mas que precisa de outro método mais específico para funcionar (addToList)
+    def addToList(v, w, e, mode): #um submétodo que faz a adição do vértice na lista auxiliar, formando a aresta
+      node = GraphAdjList.Node() #cria-se um novo objeto nó para armazenar o vértice vizinho
+        #por isso que a classe do nó somente possui declaração e visualização, as manipulações principais são da lista
+
+      node.value = w #esse novo nó possui o vértice vizinho w
+      node.edge = e #cria a variável de aresta do nó e define como a aresta passada
+      node.next = self.L[v].next #o nó criado é ligado à lista auxiliar do vértice principal específico
+        #ligado no sentido: o nó criado recebe como próximo endereço a cabeça atual da lista auxiliar
+        #isso não garante uma ligação direta (pois não se tem acesso ao novo nó através da lista)
+      self.L[v].next = node #conclui a ligação estabelendo o caminho lista principal->lista auxiliar
+        #ligado no sentido: na lista principal (de vizinhos) desse vértice recebe o nó criado
+          #logo o nó criado vira a próxima cabeça dessa lista auxiliar específica
+    #OBS: Essa parte só estabelece corretamente o sentido lista principal -> lista auxiliar, visto que é o processo de se inserir uma cabeça nova sem perder o resto do corpo
+
+      if self.doublyLinkedList: #se for aplicar com o conceito de lista duplamente encadeada (eu acho)
+      #agora não sei exatamente porque precisaria de lista duplamente encadeada em grafos
+        self.L[v].next.prev = self.L[v] #cria-se uma nova variável no nó para armazenar um anterior ao mesmo, que agora
+        #self.L[i] é um índice da lista principal, .next é a parte desse nó que guarda o "endereço" do proximo nó (sendo que self.L[i].next já existe)
+          #logo self.L[i].next.previous é uma variável nova dentro de um nó, que representa (em relaçao à um nó que se está atualmente) o nó anterior que o p´roximo nó está ligado
+            #ou seja, o próprio nó que se está atualmente porém com outras palavras, ou melhor, seguindo outro caminho:
+              #nó atual -> pula para próximo nó -> do próximo nó acessa a variável de nó anterior
+        #portanto essa linha faz especificamente essa ligação: agora confirma-se que o nó à frente está diretamente ligado com o nó atrás (permitindo a dupla ligação direta)
+          #que concretiza a ligação dupla: antes já havia a ligação
+    #OBS: Com este processo aqui que se estabelece o sentido lista auxiliar -> lista principal, principalmente pela variável prev (previous) que guarda o endereço do nó anterior a elw
+      #ao contrário de antes (que só tinha endereço para se mover para frente) tem endereço para se mover para frente e para trás
+
+        if self.L[v].next.next != None: #se o endereço que guarda o segundo nó não estiver vazio (portanto após a cabeça ainda há mais lista)
+        #considernaod que self.L[v].next agora é a nova cabeça, self.L[v].next.next seria o segundo nó da lista auxiliar após a adição da nova cabeça (e portanto é a antiga cabeça)
+        #ainda está dentro da condição de ser duplamente encadeado, então acho que é para aplicar isso nesse próximo nó
+          self.L[v].next.next.prev = self.L[v].next #logo esse segundo nó (a parte que guarda o nó anterior) vai efetivamente realizar o duplo encadeamento
+          #self.L[i].next.next é o segundo nó da lista auxiliar de um vértice v e self.L[i].next é primeiro nó (cabeça)
+
+      if self.directed: #se o grafo for direcionado, a adição na lista também precisa indicar a "direção" da vizinhança
+      #se for não direcionado essa etapa de sentido não é necessária
+        node.mode = mode #cria-se uma variável com o sentindo da aresta e adicona no nó
+
+      return node #aqui acaba o método de adicionar nó à lista na cabeça (técnica específica para adicionar o vizinho, no contexto mais geral do grafo)
+      #esse método é importante porque vai ser chamado posteriormente (dentro do método addEdge) para adicionar finalmente as viiznhanças
+      #tipo o código so SystemVerilog: não necessariamente segue a sequencia lógica das ideias escritas
+
+    #continuação do método geral para adicionar a aresta (que recebe) o nó cabeça da lista auxiliar devidamente ligado.
+      #como recebe apenas o nó, tem duas possibilidades
+        #grafo direcionado: o novo nó (nova cabeça) só tem o endereço que "aprofunda" na lista auxiliar, sem conseguir voltar para a lista principal
+        #grafo nao direcionado: a nova cabeça tem uma variável que permite "voltar" para a lista principal, então pode-se percorrer duplamente
+      #aqui que começa o início do processo (formar as arestas) para terminar em addToList (implementando as arestas nas listas)
+
+    e = GraphAdjList.Edge() #aqui se cria a aresta desejada com o __init__do inicio da classe
+      #mais especificamente a declaração das variáveis necessárias no processo de criação da aresta
+    e.v1, e.v2 = v, w #atribui os vértices em seus respectivos lugares na aresta (para formar o par)
+
+    e.node1 = addToList(v, w, e, "+") #chama o método addToList para adiconar w na lista de v (considerando v como entrada e w saída)
+    e.node2 = addToList(w, v, e, "-") #chama addToList para adicionar v na lista de w (considerando v como saída e w como entrada)
+    #essa logica de linhas é aplicado tanto para grafos direcionados quanto não direcionados:
+      #grafos não direcionados: a variável mode não é criada em cada nó, portanto a ação feita é apenas colocar um vértice na lista auxiliar do outro
+      #grafos direcionados:
+        #a primeira linha considera o par (v,w), portanto declara v como entrada e w como saída, no esquema v->w
+        #a srgunda linha considera o par (w,v), portanto considera w como saída e v como entrada, no esquema w->
+        #assim é colocada a mesma informação (em interpretações diferentes) em listas diferentes, garantindo que a relação direcionada pode ser indentificada em qualquer um dos vértices
+
+    self.m += 1 #aumenta a contagem geral de arestas em 1
+
+  def removeEdge(self, e): #método para a remoção de uma aresta
+  #nao consegui entender como eu passo a aresta como parametro na função principal (tirar essa duvida proxima semana)
+  #para a remoção de arestas (atividade específica), uma etapa necessária é a remoção de nós de uma lista (atividade geral)
+    def removeFromList(node): #método para remover nós das listas
+      node.prev.next = node.next #
+      #node é o nó que o método recebe quando é chamado (futuramente dentro de removeEdge)
+      #nde.prev é o nó anterior ao passado (dentro da lista auxiliar)
+      if node.next != None:
+        node.next.prev = node.prev
+    removeFromList(e.node1)
+    removeFromList(e.node2)
+
+    self.m -= 1 #decresce 1 na contagem geral de arestas (independente se for direcionado ou não)
+
+  def getNeighbors(self, v: int, mode: str = "*", closed: bool = False): #metodo para criar um objeto iterador que seleciona os vizinhos
+    if closed:
+      node = GraphAdjList.Node()
+      node.value = v
+
+      yield node if self.iterateOverNode else v
+
+    node = self.L[v].next
+
+    while node != None:
+      if self.directed:
+        if mode == "*" or node.mode == mode:
+          yield node if self.iterateOverNode else node.value
+      else:
+        yield node if self.iterateOverNode else node.value
+
+      node = node.next
+
+
+  def isNeighbor(self, v, w):
+    mode = "+" if self.directed else "*"
+
+    for node in self.getNeighbors(v, mode):
+      if node.value == w:
+        return True
+
+    return False
